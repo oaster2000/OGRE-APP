23f8dd5ca4b1d987be2013bbf83faafd8ac0135d
sprague-JR <41627271+sprague-JR@users.noreply.github.com>
2019-10-21 22:22:24 +0100

    minor improvements to swiping menu, still need to handle runtime orientation changes

--- New Unity Project/Assets/Scenes/Main Menu/Scripts/SwipeMenu.cs
+++ New Unity Project/Assets/Scenes/Main Menu/Scripts/SwipeMenu.cs
@@ -17,15 +17,26 @@ public class SwipeMenu : MonoBehaviour
 	public float left, top, right, bottom, delay, lerp_speed;
 
 	private float time_since_release = float.MaxValue;
-
+	private DeviceOrientation prev_orientation;
 
 	void realign()
 	{
 		for (int i = 0; i < contents.Length; i++)
 		{
+			float dim = 100;
+			Debug.Log(Input.deviceOrientation);
+
+			if(Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown || Input.deviceOrientation == DeviceOrientation.Unknown)
+			{
+				dim = contents[current].rect.width;
+
+			}else if(Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+			{
+				dim = contents[current].rect.height;
+			}
 
 				int difference = i - current;
-				float delta = (contents[current].rect.width + min_spacing) * difference;
+				float delta = (dim + min_spacing) * difference;
 				contents[i].offsetMin = new Vector2(left + delta, bottom);
 				contents[i].offsetMax = new Vector2(right + delta, top);
 				//appropriately space each element of the sliding menu
@@ -50,11 +61,26 @@ public class SwipeMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+		if ((Input.deviceOrientation == DeviceOrientation.Portrait && (prev_orientation == DeviceOrientation.LandscapeLeft || prev_orientation == DeviceOrientation.LandscapeRight))
+			|| (prev_orientation == DeviceOrientation.Portrait && (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight)))
+		{
+
+			realign();
+			
+			prev_orientation = Input.deviceOrientation;
+
+
+		}
+
+
+
+
 		float min = Mathf.Infinity;
 		current = 0;
 		for(int i = 0; i < contents.Length;i++)
 		{
-			if (Mathf.Abs(contents[i].position.x - contents[i].rect.width * 1.5f) < min)
+			if (Mathf.Abs(contents[i].position.x - (contents[i].rect.width + min_spacing)) < min)
 			{
 				current = i;
 				min = Mathf.Abs(contents[i].position.x);
